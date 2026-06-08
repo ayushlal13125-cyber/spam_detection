c0836f2acf6cfc44f5513ca595600787a4cd8de2
ayushlal13125-cyber <ayushlal13125@gmail.com>
2026-06-08 09:27:44 +0530

    updated spam_real.py

--- spam_real.py
+++ spam_real.py
@@ -41,39 +41,3 @@ print(n_pred)
 from sklearn.metrics import confusion_matrix
 print(confusion_matrix(y_test,pred))
 
-
-'''
-#pipeline
-import pandas as pd
-df=pd.read_csv('spam.csv',encoding='latin-1')
-df=df[['v1','v2']]
-df.columns=['label','text']
-from sklearn.model_selection import train_test_split
-x=df['text']
-y=df['label']
-x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,stratify=y,random_state=42)
-
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.pipeline import Pipeline
-from sklearn.feature_extraction.text import TfidfVectorizer
-import string
-
-def clean(text):
-    text=text.lower()
-    text="".join([char for char in text if char not in string.punctuation])
-    text="".join([char for char in text if not char.isdigit()])
-    return text
-
-pipeline=Pipeline([
-    ('tfidf',TfidfVectorizer(preprocessor=clean)),
-    ('model',MultinomialNB(alpha=0.5))
-])
-pipeline.fit(x_train,y_train)
-pred=pipeline.predict(x_test)
-
-from sklearn.metrics import classification_report,confusion_matrix
-print("confusion matrix",confusion_matrix(y_test,pred))
-print("classification report:",classification_report(y_test,pred))
-new=['this is my name','lottery won']
-pred_new=pipeline.predict(new)
-print(pred_new)'''
